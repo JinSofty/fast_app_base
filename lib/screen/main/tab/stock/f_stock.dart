bc0f1f50fa5003b230c698cc2ea30ecdec9f4ea4
JinSofty <144682207+JinSofty@users.noreply.github.com>
2023-11-10 17:26:59 +0900

    stock fragment sliver appbar

--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -1,3 +1,6 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/constants.dart';
+import 'package:fast_app_base/common/widget/w_image_button.dart';
 import 'package:flutter/material.dart';
 
 class StockFragment extends StatefulWidget {
@@ -10,6 +13,37 @@ class StockFragment extends StatefulWidget {
 class _StockFragmentState extends State<StockFragment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          actions: [
+            ImageButton(
+              imagePath: '$basePath/icon/stock_search.png',
+              onTap: () {
+                context.showSnackbar('검색');
+              },
+            ),
+            ImageButton(
+              imagePath: '$basePath/icon/stock_calendar.png',
+              onTap: () {
+                context.showSnackbar('일정');
+              },
+            ),
+            ImageButton(
+              imagePath: '$basePath/icon/stock_settings.png',
+              onTap: () {
+                context.showSnackbar('설정');
+              },
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: 1500,
+            color: Colors.red,
+          ),
+        )
+      ],
+    );
   }
 }
